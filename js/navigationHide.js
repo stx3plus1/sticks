8a7b425ad0e40e67dd921132e729f91b0ebc2fdb
stars6220 <bm5580737@gmail.com>
2023-03-10 21:58:46 +0000

    Testing...

--- js/navigationHide.js
+++ js/navigationHide.js
@@ -7,9 +7,11 @@ if (isMobile) {
 function scrollFunction() {
   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 25) {
     document.getElementById("sidenav").style.left = "-61px";
+    wait(500)
     document.getElementById("page").style.paddingLeft = "5px";
   } else {
     document.getElementById("sidenav").style.left = "0";
+    wait(500)
     document.getElementById("page").style.paddingLeft = "75px";
   }
 }
