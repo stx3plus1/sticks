bcf5dbaa536ce4a075807bf9262e8d5e55698c17
stars6220 <bm5580737@gmail.com>
2023-03-10 16:43:21 +0000

    OK, I'm done. Delete!

--- js/navigationHide.js
+++ js/navigationHide.js
@@ -2,7 +2,6 @@ var isMobile = window.orientation > -1;
 
 if (isMobile) {
   window.onscroll = function() {scrollFunction()};
-  window.onscroll = function() {scrollFunctionP2()};
 }
 
 function scrollFunction() {
@@ -12,13 +11,5 @@ function scrollFunction() {
   } else {
     document.getElementById("sidenav").style.left = "0";
     document.getElementById("page").style.paddingLeft = "75px";
-  }
-}
-
-function scrollFunctionP2() {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 25) {
-    document.getElementById("sidenav").style.backgroundColor = "rgba(33, 33, 40, 0.7)";
-  } else {
-    document.getElementById("sidenav").style.backgroundColor = "rgba(33, 33, 40, 0)";
   }
 }
