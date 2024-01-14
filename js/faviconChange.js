9cadb3394292373bcbe08e5bb3d6717538954c04
stx4 <80523829+stx3plus1@users.noreply.github.com>
2024-01-14 04:12:00 +0000

    Delete js directory

--- js/faviconChange.js
+++
@@ -1,9 +0,0 @@
-const faviconTag = document.getElementById("faviconTag");
-const isDark = window.matchMedia("(prefers-color-scheme: dark)");
-
-const changeFavicon = () => {
-    if (isDark.matches) faviconTag.href = "content/favicondark.png";
-    else faviconTag.href = "content/faviconlight.png";
-}
-
-changeFavicon()
