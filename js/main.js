1c834bbca724e2e1b4018587498d1a07990a2812
stx3plus1 <stx4@ballpa.in>
2024-05-08 00:58:24 +0100

    termisite

--- js/main.js
+++ js/main.js
@@ -1,62 +1,82 @@
-function start() {
-  document.getElementById('nav').style.left = "-45px";
-  document.getElementById('container').style.marginLeft = "0";
-  document.getElementById('newline').style.display = "none";
+function scrolltop() {
+    var objDiv = document.getElementById("html");
+    objDiv.scrollTop = objDiv.scrollHeight;
 }
-
-function showdiv1() {
-  document.getElementById('hide1').style.visibility = "visible";
-  document.getElementById('cursor1').style.display = "none";
-  document.getElementById('cursor2').style.visibility = "visible";
+function parse() {
+    if(event.key === 'Enter') {
+        newLineOfOutput()
+    }
 }
-function showdiv2() {
-  document.getElementById('hide2').style.visibility = "visible";
-  document.getElementById('cursor2').style.display = "none";
+function newLineOfOutput() {
+    if (document.getElementById("shell").value == "") {
+        return;
+    }
+    if (document.getElementById("shell").value == "clear") {
+        while (document.getElementById("text")) {
+            document.getElementById("text").remove();
+        }
+        document.getElementById("shell").value = "";
+        return;
+    }
+    const p = document.createElement("p");
+    const parent = document.getElementById("prompt");
+    p.id = "text";
+
+    if (document.getElementById("shell").value == "help") {
+        p.innerHTML = "$ " + document.getElementById("shell").value + "<br><h1>Help</h1>Let's get you some help.<br><br>ls - list all files (fake)<br>help - this command<br>about - about me!<br>projects - some of my projects<br>clear - works as in Linux<br><br>More will come eventually...";
+        parent.appendChild(p);
+        document.getElementById("shell").value = "";
+        scrolltop();
+        return;
+    }
+    if (document.getElementById("shell").value == "about") {
+        let text = `
+        <h1>About</h1>
+        Hi, I'm cirkul/stx! Currently a terrible programmer living in Hell Censorship 2.0, legally the United Kingdom. 
+        <br>I enjoy C, HTML + CSS, JavaScript and fucking around.
+        <br>I don't have anything special to show off here, but I can say myself that I am quite the idiot.
+        <br>I mean... I didn't hack an Amazon Fire at 4 years old... at all.
+        `
+        p.innerHTML = "$ " + document.getElementById("shell").value + "<br>" + text;
+        parent.appendChild(p);
+        document.getElementById("shell").value = "";
+        scrolltop();
+        return;
+    }
+    if (document.getElementById("shell").value == "projects") {
+        let text = `
+        <h1>Projects</h1>
+        Currently, I'm working on a few, pretty good projects - including this website.
+        <br>My best so far is strfetch, a system information tool - sort of like neofetch (RIP) that gives stats nicely.
+        <br>However, it shows a stupid little string, so I'm special and I win of course.
+        <br><small>I use arch btw (editors note (also me) i use debian lol)</small>
+        <br>Some of my older projects include sillyfetch (not public yet) and Windows Limeover.
+        <br>Limeover is a collection of Windows 7 modifications to make it a fresh lime theme... but it's a bit dead since M4 has been in development for literal years.
+        <br>It's whatever, I prefer C over Resource Hacker now.
+        <br>Anyways, enough of my random stuff. Go do something productive!
+        `
+        p.innerHTML = "$ " + document.getElementById("shell").value + "<br>" + text;
+        parent.appendChild(p);
+        document.getElementById("shell").value = "";
+        scrolltop();
+        return;
+    }
+    if (document.getElementById("shell").value == "strfetch") {
+        p.innerHTML = "$ " + document.getElementById("shell").value + "<br>You're funny.";
+        parent.appendChild(p);
+        document.getElementById("shell").value = "";
+        scrolltop();
+        return;
+    }
+    if (document.getElementById("shell").value == "ls") {
+        p.innerHTML = "$ " + document.getElementById("shell").value + "<br>404.html &nbsp css &nbsp home.png &nbsp index.html &nbsp js &nbsp projects.html &nbsp README.md";
+        parent.appendChild(p);
+        document.getElementById("shell").value = "";
+        scrolltop();
+        return;
+    }
+    p.innerHTML = "$ " + document.getElementById("shell").value + "<br>Unknown command: " + document.getElementById("shell").value + ", try 'help'";
+    parent.appendChild(p);
+    document.getElementById("shell").value = "";
+    scrolltop();
 }
-
-function initHide() {
-  document.getElementById('main').style.opacity = "0";
-  document.getElementById('nav').style.transition = "0.5s ease-in-out";
-  document.getElementById('nav').style.left = "0px";
-}
-
-function showFinal() {
-  // lengthy function
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-
-  document.getElementById('login').style.display = "none";
-  document.getElementById('titletext').innerHTML = "Browser";
-  if (w < 650) {
-    document.getElementById('titlebar').style.width = "90vw";
-    document.getElementById('titlebar').style.marginLeft = "40px";
-  } else {
-    document.getElementById('container').style.paddingLeft = "25px";
-  }
-  document.getElementById('main').style.opacity = "1";
-  document.getElementById('postlog').style.visibility = "visible";
-}
-
-function newline() {
-   document.getElementById('newline').style.display = "block";
-}
-
-var i = 0;
-var txt = './sticks.sh -v'; 
-var speed = 50; 
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("type").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
-setTimeout("start()", 1);
-setTimeout("showdiv1()", 1000);
-setTimeout("typeWriter()", 2000);
-setTimeout("newline()", 2900);
-setTimeout("showdiv2()", 3500);
-setTimeout("initHide()", 5000);
-setTimeout("showFinal()", 6000);
